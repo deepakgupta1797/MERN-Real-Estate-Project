036f4ecd7625f4e6cc63a73efdd89696ebe97504
deepakgupta1797 <deepakgupta1797@gmail.com>
2023-11-17 16:50:25 +0530

    changes

--- api/index.js
+++ api/index.js
@@ -2,6 +2,7 @@ import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 import userRouter from './routes/user.route.js';
+import authRouter from './routes/auth.route.js';
 dotenv.config();
 
 // Connection to mongodb
@@ -23,3 +24,4 @@ app.listen(3000, () => {
 
 // test api using json
 app.use("/api/user", userRouter);
+app.use("/api/auth", authRouter);
